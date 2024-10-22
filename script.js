dc20d9f653f487b9fcd65c8e9a55234f0c6dd7ff
saeed rahimi <saeedteachcodee@email.com>
2024-10-22 18:49:50 +0330

    fix

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
 "use strict"
- 
-const body=document.body;
-body.style.padding="0";
-body.style.margin="0";
-body.style.boxSizing="border-box";
-const html=` <div class="menu">
-        <ul class="list">
-            <li class="item">saeed</li>
-            <li class="item">saeed</li>
-            <li class="item">saeed</li>
-            <li class="item">saeed</li>
-            <li class="item">saeed</li>
-        </ul>
-    </div>`;
-    body.insertAdjacentHTML('afterbegin',html)
+
+const container = document.createElement("div");
+const navbar = document.createElement("div");
+const product = document.createElement("div");
+const searchBar = document.createElement("div");
+const boxs=document.createElement("div");
+const boxlist=document.createElement("ul");
+const boxitem=document.createElement("li");
+const input=document.createElement("input")
+// Add classes
+container.classList.add("container");
+navbar.classList.add("navbar");
+product.classList.add("product");
+boxlist.append(boxitem)
+boxs.append(boxlist)
+console.log(boxs);
+
+
+// Append the navbar and product to the container
+container.append(navbar);
+container.append(product);
+navbar.append(input);
+
+
+// style input
+input.style.margin="20px"
+input.style.width="80%"
+input.style.height="50px"
+input.style.backgroundColor="red";
+input.style.border=" 2px solid blue"
+input.style.borderRadius="10px"
+input.style.fontSize="30px"
+input.style.padding="15px"
+input.maxLength="15"
+input.placeholder="enter your Food"
+
+
+
+// Style the container
+container.style.width = "100%";
+container.style.display = "flex";
+container.style.backgroundColor = "black";
+document.body.append(container); // Make sure to append container to the document body to make it visible
+
+// Style the navbar
+navbar.style.width = "30%";
+navbar.style.height = "80vh";
+navbar.style.backgroundColor = "white";
+navbar.style.margin = "40px";
+navbar.style.marginTop = "135px";
+navbar.style.borderRadius = "16px";
+
+// Style the product div
+product.style.width = "70%";
+product.style.height = "80vh";
+product.style.backgroundColor = "green";
+product.style.margin = "40px";
+product.style.marginTop = "135px";
+product.style.borderRadius = "16px";

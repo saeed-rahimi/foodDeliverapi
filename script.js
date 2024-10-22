0b791d6a9c58577d9713225cae92b5e031c0f151
saeed rahimi <saeedteachcodee@email.com>
2024-10-22 15:16:42 +0330

    fixbase

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 "use strict"
  
 const body=document.body;
-const html=``
+body.style.padding="0";
+body.style.margin="0";
+body.style.boxSizing="border-box";
+const html=` <div class="menu">
+        <ul class="list">
+            <li class="item">saeed</li>
+            <li class="item">saeed</li>
+            <li class="item">saeed</li>
+            <li class="item">saeed</li>
+            <li class="item">saeed</li>
+        </ul>
+    </div>`;
+    body.insertAdjacentHTML('afterbegin',html)
